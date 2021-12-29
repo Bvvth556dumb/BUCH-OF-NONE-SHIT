6e47fb31ec1fa911e1775bab54935fe181441105
Noobandguestgang <95111164+Noobandguestgang@users.noreply.github.com>
2021-12-29 20:19:25 +0700

    Add files via upload

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -185,7 +185,7 @@ class MainMenuState extends MusicBeatState
 			{
 				if (optionShit[curSelected] == 'donate')
 				{
-					CoolUtil.browserLoad('https://ninja-muffin24.itch.io/funkin');
+					CoolUtil.browserLoad('https://youtube.com/c/NonsenseHumorLOL');
 				}
 				else
 				{
